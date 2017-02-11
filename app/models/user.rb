e683d86bee6871916c53e6f8d0cc64c031ea8b7e
ixxxxxx <u.lostland@gmail.com>
2017-02-11 10:37:48 +0800

    use username or email to login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :login
+
   validates :username, :presence => true, :uniqueness => {:case_sensitive => false}, 
                        :format => {:with => /\A[A-Za-z0-9\-\_\.]+\z/, :message => I18n.t('errors.messages.space_name') }, 
                        :length => {:in => 4..20}
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_hash).first
+    end
+  end
 end
